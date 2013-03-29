26c83e71da6d7dc4d1169b3a1e9bd1a5da378f47
Mike Hook <hook.mike@gmail.com>
2013-03-29 10:35:14 +0000

    First run - completed by following instructions.

--- BowlingKata.Core/Game.cs
+++ BowlingKata.Core/Game.cs
@@ -7,5 +7,50 @@ namespace BowlingKata.Core
 {
 	public class Game
 	{
+		private int[] rolls = new int[21];
+		private int currentRoll = 0;
+
+		public void Roll(int pins)
+		{		
+			rolls[currentRoll++] = pins;
+		}
+
+		public int Score() 
+		{
+			int score = 0;
+			int frameIndex = 0;
+			for (int frame = 0; frame < 10; frame++)
+			{
+				if (isStrike(frameIndex)) 
+				{
+					score += 10 + rolls[frameIndex + 1] + rolls[frameIndex + 2];
+					frameIndex ++;
+				}				
+				else if (isSpare(frameIndex)) 
+				{
+					score += 10 + rolls[frameIndex + 2];
+					frameIndex += 2;
+				}
+				else
+				{
+					score += rolls[frameIndex] + rolls[frameIndex + 1];
+					frameIndex += 2;
+				}		
+
+			}
+
+			return score;
+		}
+
+		private bool isStrike(int frameIndex)
+		{
+			return rolls[frameIndex] == 10;
+		}
+
+		private bool isSpare(int frameIndex)
+		{
+			return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
+		}
+
 	}
 }
--- BowlingKata.Tests/GameTests.cs
+++ BowlingKata.Tests/GameTests.cs
@@ -2,10 +2,84 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using BowlingKata.Core;
+using NUnit.Framework;
 
 namespace BowlingKata.Tests
 {
+	[TestFixture]
 	public class GameTests
 	{
+		private Game game;
+
+		private void rollMany(int n, int pins)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				game.Roll(pins);
+			}
+		}
+
+		private void rollSpare()
+		{
+			game.Roll(5);
+			game.Roll(5); //spare
+		}
+
+		private void rollStrike()
+		{
+			game.Roll(10); //strike
+		}
+
+		[SetUp]
+		protected void SetUp()
+		{
+			game = new Game();
+		}
+
+		[Test]
+		public void TestGutterGame()
+		{
+			rollMany(20, 0);
+			Assert.That(game.Score(), Is.EqualTo(0));
+		}
+
+		[Test]
+		public void TestAllOnes()
+		{
+			rollMany(20, 1);
+			Assert.That(game.Score(), Is.EqualTo(20));
+		}
+
+		[Test]
+		public void TestOneSpare()
+		{
+			rollSpare();
+			game.Roll(3);
+			rollMany(17, 0);
+
+			Assert.That(game.Score(), Is.EqualTo(16));
+		}
+
+		[Test]
+		public void TestOneStrike()
+		{
+			rollStrike();
+			game.Roll(3);
+			game.Roll(4);
+			rollMany(16, 0);
+
+			Assert.That(game.Score(), Is.EqualTo(24));
+		}
+
+		[Test]
+		public void TestPerfectGame()
+		{			
+			rollMany(12, 10);
+			Assert.That(game.Score(), Is.EqualTo(300));
+		}
+
+
+		
 	}
 }
